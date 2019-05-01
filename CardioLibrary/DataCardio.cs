9c44697ded4da2cc791da8f46c0c7610e997107f
Matteo Uras <Ursambriaco@hotmail.com>
2019-05-01 19:25:11 +0200

    Creazione di 5 Script i quali chiedevano di calcolare: - battiti minimi e massimi - valori frequenza cardiaca - calorie bruciate durante una sessione di esercizi - spesa energetica in caso di corsa o di camminata - media giornaliera di battiti  cardiaci

---
+++ CardioLibrary/DataCardio.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CardioLibrary
+{
+    public class DataCardio
+    {
+        //Script 1
+        public static double battiti_minimi(int età)
+        {
+            int frequenza = 220 - età;
+            double battiti_minimi = frequenza * 0.7;
+            return battiti_minimi;
+        }
+
+        public static double battiti_massimi(int età)
+        {
+            int frequenza = 220 - età;
+            double battiti_massimi = frequenza * 0.9;
+            return battiti_massimi;
+        }
+
+        //Script 2
+        public static string FrequenzaCardiacaRiposo(double battiti)
+        {
+            string risultato = "";
+
+            if (battiti <= 60)
+            {
+                risultato = "bradicardia";
+            }
+
+            if (battiti >= 60 && battiti <= 100)
+            {
+                risultato = "normale";
+            }
+
+            if (battiti > 100)
+            {
+                risultato = "tachicardia";
+            }
+
+            return risultato;
+        }
+
+        //Script 3
+        public static double Caloriebruciate(string sesso, double frequenza_cardiaca, double peso, int anni, double durata_sessione_allenamento)
+        {
+            double calorie_bruciate = 0;
+
+            if (sesso == "M")
+            {
+                calorie_bruciate = ((anni * 0.2017) + (peso * 0.199) + (frequenza_cardiaca * 0.6309) - 55.0969) * durata_sessione_allenamento / 4.184;
+                calorie_bruciate = Math.Round(calorie_bruciate, 2);
+            }
+
+            if (sesso == "F")
+            {
+                calorie_bruciate = ((anni * 0.074) - (peso * 0.126) + (frequenza_cardiaca * 0.4472) - 20.4022) * durata_sessione_allenamento / 4.184;
+                calorie_bruciate = Math.Round(calorie_bruciate, 2);
+            }
+
+            return calorie_bruciate;
+        }
+
+        //Script 4
+        public static double SpesaEnergetica(string attività, double km_percorsi, double peso_corporeo)
+        {
+            double r = 0;
+
+            if (attività == "Corsa")
+            {
+                r = (0.9 * km_percorsi) * peso_corporeo;
+            }
+
+            if (attività == "Camminata")
+            {
+                r = (0.50 * km_percorsi) * peso_corporeo;
+            }
+
+            return r;
+        }
+
+        //Script 5 
+        public static double battitigiornalieri(double bat_risposo, double bat_massimi, double bat_recupero)
+        {
+            double Bat_giornalieri = 0;
+
+            Bat_giornalieri = (bat_risposo + bat_massimi + bat_recupero) / 3;
+            Bat_giornalieri = Math.Round(Bat_giornalieri, 1);
+
+            return Bat_giornalieri;
+
+        }
+    }
+}
---
+++ DataCardio.Test/UnitTest1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DataCardio.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        //Script 1
+        [DataTestMethod]
+        [DataRow(15, 250, 175, 225)] //Calcoliamo battiti minimi a massimi facendo: battiti minimi * 0.7 e battiti massimi per 0.9
+        [DataRow(20, 200, 140, 180)]
+        [DataRow(25, 195, 136.5, 175.5)]
+        [DataRow(30, 190, 133, 171)]
+        [DataRow(35, 185, 129.5, 166.5)]
+        public void TestMethod1(int età, int frequenza, double battiti_minimi_aspettati, double battiti_massimi_aspettati)
+        {
+            double battiti_minimi_calcolati = CardioLibrary.DataCardio.battiti_minimi(età);
+            Assert.AreEqual(battiti_minimi_calcolati, battiti_minimi_aspettati);
+
+            double battiti_massimi_calcolati = CardioLibrary.DataCardio.battiti_massimi(età);
+            Assert.AreEqual(battiti_massimi_calcolati, battiti_massimi_aspettati);
+        }
+
+        //Script 2
+        [DataTestMethod]
+        [DataRow(45, "bradicardia")]
+        [DataRow(70, "normale")]
+        [DataRow(90, "normale")]
+        [DataRow(40, "bradicardia")]
+        [DataRow(55, "bradicardia")]
+        [DataRow(110, "tachicardia")]
+        [DataRow(120, "tachicardia")]
+
+        public void TestValoriFrequenzaCardiacaRiposo(double battiti, string frequenza_cardiaca_riposo_aspettata)
+        {
+            string frequenza_cardiaca_riposo = CardioLibrary.DataCardio.FrequenzaCardiacaRiposo(battiti);
+            Assert.AreEqual(frequenza_cardiaca_riposo, frequenza_cardiaca_riposo_aspettata);
+        }
+
+        //Script 3
+        [DataTestMethod]
+        [DataRow("M", 90, 75, 40, 60, 125.4)] //Sesso, frequenza cardiaca media, peso, anni, durata sessione allenamento, calorie bruciate
+        [DataRow("F", 75, 55, 35, 40, 50.2)]
+
+        public void TestCalorieBruciate(string sesso, double frequenza_cardiaca, double peso, int anni, double durata_sessione_allenamento, string calorie_bruciate_aspettate)
+        {
+            double calorie_bruciate = CardioLibrary.DataCardio.Caloriebruciate(sesso, frequenza_cardiaca, peso, anni, durata_sessione_allenamento);
+            Assert.AreEqual(calorie_bruciate_aspettate, calorie_bruciate);
+        }
+
+        //Script 4
+        [DataTestMethod]
+        [DataRow("Corsa", 5, 50, 600)] //Attività, Km percorsi, peso corporeo, spesa energetica
+        [DataRow("Camminata", 10, 60, 450)]
+
+        public void TestSpesaEnergetica(string attività, double km_percorsi, double peso_corporeo, double SpesaEnergetica, double spesa_energetica_aspettata)
+        {
+            double Spesa_Energetica = CardioLibary.DataCardio.SpesaEnergetica(attività, km_percorsi, peso_corporeo);
+            Assert.AreEqual(spesa_energetica_aspettata, SpesaEnergetica);
+        }
+
+        //Script 5
+        [DataTestMethod]
+        [DataRow(75, 120, 110, 100)] //Battiti riposo, battiti massimi, battiti recupero, media giornaoliera
+        [DataRow(65, 110, 100, 90)]
+
+        public void TestBatGiornalieri (double bat_riposo, double bat_massimi, double bat_recupero, double bat_giornalieri_aspettati)
+        {
+            double bat_giornalieri = CardioLibrary.DataCardio.battitigiornalieri(bat_riposo, bat_massimi, bat_recupero);
+            Assert.AreEqual(bat_giornalieri_aspettati, bat_giornalieri);
+        }
+    }
+}
+ 
